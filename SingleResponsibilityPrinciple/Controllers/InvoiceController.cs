bb28dfbd515e4e709a0a8fd966941202339442f0
John Scatena <johne5@yahoo.com>
2021-02-11 07:39:26 -0500

    Create JScatena branch

--- SingleResponsibilityPrinciple/Controllers/InvoiceController.cs
+++ SingleResponsibilityPrinciple/Controllers/InvoiceController.cs
@@ -57,40 +57,14 @@ namespace SingleResponsibilityPrinciple.Controllers
             return View();
         }
 
-        public bool SendEmailNotificationForInvoice(Invoice invoice)
-        {
-            //send an email notification
-            //try
-            //{
-                //SMTPService service = new SMTPService()
-                //{
-                //    FromEmail = "noreply@srp.com",
-                //    Subject = "Your new invoice has been entered!",
-                //    Body = "Invoice Number 23243 has been....."
-                //};
-                //service.SendEmail();
-            //}
-            //catch(Exception ex)
-            //{
-            //    LogError(ex);
-            //}
-
-            return true;
-        }
+        
 
         private void LogError()
         { 
             //log error here
         }
 
-        public class Invoice
-        {
-            public int InvoiceNumber { get; set; }
-            public decimal InvoiceAmount { get; set; }
-            public string Requester { get; set; }
-            public string RequesterEmail { get; set; }
-            public bool IsActive { get; set; }
-        }
+        
 
     }
 }
