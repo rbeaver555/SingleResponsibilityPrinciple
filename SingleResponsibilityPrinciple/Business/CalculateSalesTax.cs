23cbe1116784926ecae4b79d61b5fbe1a95fef43
rbeaver555 <48887728+rbeaver555@users.noreply.github.com>
2021-03-14 13:40:50 -0500

    Open Closed Principle

---
+++ SingleResponsibilityPrinciple/Business/CalculateSalesTax.cs
@@ -0,0 +1,25 @@
+﻿using SingleResponsibilityPrinciple.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple.Business
+{
+    public class CalculateSalesTax
+    {
+
+        public decimal CalculateTotalTaxOnInvoices(IEnumerable<Invoice> invoices)
+        {
+            decimal totalTax = 0;
+
+            foreach(Invoice inv in invoices)
+            {
+                totalTax += inv.InvoiceAmount * .08M;
+            }
+
+            return totalTax;
+        }
+
+    }
+}
